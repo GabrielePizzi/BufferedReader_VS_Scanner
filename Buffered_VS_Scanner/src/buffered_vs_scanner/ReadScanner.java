a77436877ef11ac8f249bd1c31fe65d75cf1ec54
pizzi.gabriele <pizzi.gabriele@BIBLIO-PC02.OSDBSESTO.local>
2020-01-31 12:51:40 +0100

    added methods

--- Buffered_VS_Scanner/src/buffered_vs_scanner/ReadScanner.java
+++ Buffered_VS_Scanner/src/buffered_vs_scanner/ReadScanner.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package buffered_vs_scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  *
  * @author pizzi.gabriele
  */
 public class ReadScanner {
-    
+
+    public static void readScanner() throws FileNotFoundException {
+        //creating File instance to reference text file in Java
+        File text = new File("C:/temp/test.txt");
+
+        //Creating Scanner instnace to read File in Java
+        Scanner scnr = new Scanner(text);
+
+        //Reading each line of file using Scanner class
+        int lineNumber = 1;
+        while (scnr.hasNextLine()) {
+            String line = scnr.nextLine();
+            System.out.println("line " + lineNumber + " :" + line);
+            lineNumber++;
+        }
+    }
 }
